81b2c5236e0b4d17e411b0f1ccf22822d5e9770d
nissebergman <nibe91@gmail.com>
2021-04-08 13:37:36 +0200

    Pushing debug instead

---
+++ LAB1/cmake-build-release/CMakeFiles/LAB1WNODE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB1WNODE.dir/Puzzle.cpp.o"
+  "CMakeFiles/LAB1WNODE.dir/main.cpp.o"
+  "LAB1WNODE"
+  "LAB1WNODE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LAB1WNODE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
